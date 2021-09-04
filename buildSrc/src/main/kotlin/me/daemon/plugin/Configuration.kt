6ad068b6c360e0cd6a53f096b75962f4bd0d1ff0
daemon369 <daemon336699@gmail.com>
2021-09-04 15:24:59 +0800

    create project and modules

---
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -0,0 +1,9 @@
+package me.daemon.plugin
+
+object Configuration {
+    const val minSdkVersion = 19
+    const val compileSdkVersion = 30
+    const val targetSdkVersion = 23
+    const val buildToolsVersion = "30.0.3"
+    const val groupId = "io.github.daemon369"
+}
