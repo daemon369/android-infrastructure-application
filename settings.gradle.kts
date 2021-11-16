88f555bcc4e7efc5a2d52a1dfca98d903d9a1ae4
daemon369 <daemon336699@gmail.com>
2021-11-17 06:36:41 +0800

    upgrade android-version-catalog to 0.0.5; upgrade gradle to 7.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,13 @@ enableFeaturePreview("VERSION_CATALOGS")
 
 dependencyResolutionManagement {
     repositories {
+//        mavenLocal()
         mavenCentral()
         maven { url = uri("https://s01.oss.sonatype.org/content/repositories/releases/") }
     }
     versionCatalogs {
         create("libs") {
-            from("io.github.daemon369:android-version-catalog:0.0.4")
+            from("io.github.daemon369:android-version-catalog:0.0.5")
         }
     }
 }
