db531b1c110779cbfebbbce3a6582e690eee7147
daemon369 <daemon336699@gmail.com>
2021-11-19 07:12:34 +0800

    upgrade compileSdkVersion and buildToolsVersion due to error:     The minCompileSdk (31) specified in a                                                                   dependency's AAR metadata (META-INF/com/android/build/gradle/aar-metadata.properties)                                                                   is greater than this module's compileSdkVersion (android-30).                                                                   Dependency: androidx.core:core-ktx:1.7.0.                                                                   AAR metadata file: /Users/y/.gradle/caches/transforms-3/9ea5515ec4e3ce80bc23011035ad4c3f/transformed/jetified-core-ktx-1.7.0/META-INF/com/android/build/gradle/aar-metadata.properties.

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -2,8 +2,8 @@ package me.daemon.plugin
 
 object Configuration {
     const val minSdkVersion = 19
-    const val compileSdkVersion = 30
+    const val compileSdkVersion = 31
     const val targetSdkVersion = 23
-    const val buildToolsVersion = "30.0.3"
+    const val buildToolsVersion = "31.0.0"
     const val groupId = "io.github.daemon369"
 }
