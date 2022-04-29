a3e7aa0a2ac5155eeb37cd0885531ffd92d83a1f
daemon369 <daemon336699@gmail.com>
2022-04-29 14:47:25 +0800

    refactor, move sonatype properties initialization to init script

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,4 +13,12 @@ tasks.create<Delete>("clean") {
     delete(rootProject.buildDir)
 }
 
-apply(from = "$rootDir/gradle/publish-root.gradle")
+//allprojects {
+//    println("project->${this.name}")
+//    println("ext.ossrhUsername=${ext["ossrhUsername"]}")
+//    println("ext.ossrhPassword=${ext["ossrhPassword"]}")
+//    println("ext.sonatypeStagingProfileId=${ext["sonatypeStagingProfileId"]}")
+//    println("ext.signing.keyId=${ext["signing.keyId"]}")
+//    println("ext.signing.password=${ext["signing.password"]}")
+//    println("ext.signing.secretKeyRingFile=${ext["signing.secretKeyRingFile"]}")
+//}
